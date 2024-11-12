f63b234ddca48228f9411562e8640faf9267bd77
MP <paulmbui20@gmail.com>
2024-11-12 11:36:31 +0300

    added dropdown menu in navbar for large screen

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    speed: 500,
-    spaceBetween: 100,
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-    // pagination: {
-    //     el: '.swiper-pagination',
-    //   },
-      grabCursor: true,
-       // Navigation arrows
+const swiper = new Swiper(".swiper", {
+  speed: 500,
+  spaceBetween: 100,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+  grabCursor: true,
+  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-
-  });
-swiper.slideNext()
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+swiper.slideNext();
 //back to top button code
 const mobileMenu = document.getElementById("mobile-menu");
 const menuItems = document.getElementById("menu-items");
@@ -30,19 +29,31 @@ function scrollToTop() {
 }
 //darkmode toggle
 function darkmode() {
-    document.body.classList.toggle("darkmode");
- }
+  document.body.classList.toggle("darkmode");
+}
 
- function menutoggle(){
+function menutoggle() {
   const hamburgerIcon = document.getElementById("hamburgerIcon");
   menu = document.querySelector(".menu-content");
   menu.classList.toggle("hidden");
-  if (menu.classList.contains("hidden")){
+  if (menu.classList.contains("hidden")) {
     hamburgerIcon.classList.remove("fa-times");
     hamburgerIcon.classList.add("fa-bars");
-
-  } else{
+  } else {
     hamburgerIcon.classList.remove("fa-bars");
     hamburgerIcon.classList.add("fa-times");
   }
- }
+}
+
+const dropdown = document.getElementById("dropdown");
+const dropdownItems = document.getElementById("dropdown-items");
+dropdown.addEventListener("mouseover", hover);
+dropdownItems.addEventListener("mouseover", hover);
+dropdownItems.addEventListener("mouseleave", hoverhide);
+dropdown.addEventListener("click", hover);
+function hoverhide() {
+  dropdownItems.classList.add("hidden");
+}
+function hover() {
+  dropdownItems.classList.toggle("hidden");
+}
